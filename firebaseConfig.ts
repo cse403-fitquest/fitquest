2114717629bdcaa8089cebbab8433083f9263fef
Muhammad Afuzarahman <theafuza@gmail.com>
2024-10-22 20:20:21 -0700

    Add unit tests for FQButton and FQTextInput

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyDZq4Z6931YsBCyafLXMM9ZtdqZWlyhXro',
