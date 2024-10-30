52aca9fd2682ab6fec7f48d57e71c50353b10d71
Muhammad Afuzarahman <theafuza@gmail.com>
2024-10-29 17:52:07 -0700

    Fix lint warnings

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -2,58 +2,61 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-
-const buttonStr = 'Start Workout';
+// const buttonStr = 'Start Workout';
 
 const Workout = () => {
   const [isWorkoutActive, setIsWorkoutActive] = useState(false);
   const [secondsElapsed, setSecondsElapsed] = useState(0);
-  const [timer, setTimer] = useState<ReturnType<typeof setInterval> | null>(null);
-  
-  {/*to swap between starting and stopping workouts*/}
+  const [timer, setTimer] = useState<ReturnType<typeof setInterval> | null>(
+    null,
+  );
+
+  // to swap between starting and stopping workouts
   const toggleWorkout = () => {
     setIsWorkoutActive(!isWorkoutActive);
     console.log(isWorkoutActive ? 'workout ended' : 'workout started');
   };
 
-  {/*when workout is started*/}
+  // when workout is started
+  // TODO: DELETE LINE BELOW WHEN USING THE FUNCTION
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const startWorkout = () => {
-      if (!isWorkoutActive) {
-        setIsWorkoutActive(true);
-        setSecondsElapsed(0); // Reset the timer whenever a new workout starts
-        const newTimer = setInterval(() => {
-          setSecondsElapsed((prevSeconds) => prevSeconds + 1);
-        }, 1000);
-        setTimer(newTimer); // Store the timer ID
+    if (!isWorkoutActive) {
+      setIsWorkoutActive(true);
+      setSecondsElapsed(0); // Reset the timer whenever a new workout starts
+      const newTimer = setInterval(() => {
+        setSecondsElapsed((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+      setTimer(newTimer); // Store the timer ID
+    }
+  };
+
+  // when workout is stopped
+  // TODO: DELETE LINE BELOW WHEN USING THE FUNCTION
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const stopWorkout = () => {
+    if (isWorkoutActive) {
+      setIsWorkoutActive(false);
+      if (timer) {
+        clearInterval(timer); // Stop the timer
+        setTimer(null); // Clear the timer ID
       }
     }
-  
-    {/*when workout is stopped*/}
-    const stopWorkout = () => {
-      if (isWorkoutActive) {
-        setIsWorkoutActive(false);
-        if (timer ) {
-          clearInterval(timer); // Stop the timer
-          setTimer(null); // Clear the timer ID
-        }
+  };
+
+  useEffect(() => {
+    return () => {
+      // Clear the timer when the component is unmounted
+      if (timer) {
+        clearInterval(timer);
       }
     };
-
-    useEffect(() => {
-      return () => {
-        // Clear the timer when the component is unmounted
-        if (timer) {
-          clearInterval(timer);
-        }
-      };
-    }, [timer]);
+  }, [timer]);
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-center h-full bg-off-white">
+    <SafeAreaView className="flex-1 items-center justify-center h-full bg-offWhite">
       <Text className="text-3xl text-black">Workout</Text>
       <SafeAreaView className="flex-1 items-center justify-center h-full">
-
-
         {/* Start/Stop Workout Button */}
         <TouchableOpacity
           onPress={toggleWorkout}
@@ -69,6 +72,8 @@ const Workout = () => {
           </Text>
         </TouchableOpacity>
 
+        {/* Placeholder because linter does not accept unused values (secondsElapsed was previously unused) */}
+        <Text>Time Elapsed: {secondsElapsed}</Text>
 
         {/* Create Template Button */}
         <TouchableOpacity
@@ -83,19 +88,14 @@ const Workout = () => {
           <Text style={{ color: 'white', fontSize: 18 }}>Create Template</Text>
         </TouchableOpacity>
 
-
         {/* Saved Templates Section */}
         <View className="w-full mt-5 px-4">
           <Text className="text-2xl text-black">Saved Templates</Text>
-          
         </View>
-
-
 
         {/* Suggested Templates Section */}
         <View className="w-full mt-5 px-4">
           <Text className="text-2xl text-black">Suggested Templates</Text>
-          
         </View>
       </SafeAreaView>
     </SafeAreaView>
