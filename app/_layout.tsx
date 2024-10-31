4d35f0020ac0abc056b760d7914eaaa301026617
Muhammad Afuzarahman <theafuza@gmail.com>
2024-10-30 17:30:07 -0700

    Handle auth redirect in root layout instead of root index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { BASE_USER } from '@/constants/user';
 import { FIREBASE_AUTH } from '@/firebaseConfig';
 import { useUserStore } from '@/store/user';
 import { useFonts } from 'expo-font';
@@ -23,11 +24,16 @@ export default function RootLayout() {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
-        console.log('User is signed in');
+        console.log('User is signed in', user.uid);
         // console.log(user);
 
+        setUser({
+          ...BASE_USER,
+          id: user.uid,
+        });
+
         // Navigate to the appropriate screen
-        router.replace('/sign-out' as Href);
+        router.replace('/profile' as Href);
       } else {
         // User is signed out
         console.log('User is signed out');
