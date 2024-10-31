c20bb1dc1afb523c322655f8627717cec17acbed
Muhammad Afuzarahman <theafuza@gmail.com>
2024-10-30 17:41:23 -0700

    Remove unused tab screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Href, Redirect, Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
+import { Colors } from '@/constants/colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { StatusBar } from 'expo-status-bar';
 import { isLoggedIn } from '@/utils/auth';
@@ -29,21 +29,6 @@ export default function TabLayout() {
           },
         }}
       >
-        {/* Profile */}
-        <Tabs.Screen
-          name="sign-out"
-          options={{
-            title: 'Sign Out',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? 'body' : 'body-outline'}
-                color={color}
-                size={20}
-              />
-            ),
-          }}
-        />
-
         {/* Profile */}
         <Tabs.Screen
           name="profile"
@@ -118,31 +103,6 @@ export default function TabLayout() {
             ),
           }}
         />
-
-        {/* <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? 'home' : 'home-outline'}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? 'code-slash' : 'code-slash-outline'}
-              color={color}
-            />
-          ),
-        }}
-      /> */}
       </Tabs>
       <StatusBar backgroundColor="#161622" style="light" />
     </>
