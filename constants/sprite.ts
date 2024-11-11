b373ff459a7906514b796cc1667511eb5feaf09e
Prayug <prayugsigdel@gmail.com>
2024-11-10 16:39:53 -0800

    cleaned up

--- constants/sprite.ts
+++ constants/sprite.ts
@@ -3,25 +3,28 @@ export enum SpriteID {
   T1_DAGGER = 't1_dagger',
   T1_SWORD = 't1_sword',
   T1_HELM = 't1_helm',
+  T1_RING = 't1_ring',
   T1_SHIELD = 't1_shield',
   T1_HEAVY_ARMOR = 't1_heavy_armor',
 
   T2_DAGGER = 't2_dagger',
   T2_SWORD = 't2_sword',
-  //   T2_HELM = 't2_helm',
-  //   T2_SHIELD = 't2_shield',
+  T2_HELM = 't2_helm',
+  T2_SHIELD = 't2_shield',
   T2_HEAVY_ARMOR = 't2_heavy_armor',
 
   T3_DAGGER = 't3_dagger',
   T3_SWORD = 't3_sword',
+  T3_HEAVY_ARMOR = 't3_heavy_armor',
 
   T4_DAGGER = 't4_dagger',
   T4_SWORD = 't4_sword',
+  T4_HEAVY_ARMOR = 't4_heavy_armor',
 
   //  Consumables
-  //   SMALL_HEALTH_POTION = 'small_health_potion',
-  //   MEDIUM_HEALTH_POTION = 'medium_health_potion',
-  //   LARGE_HEALTH_POTION = 'large_health_potion',
+  HEALTH_POTION_SMALL = 'health_potion_small',
+  HEALTH_POTION_MEDIUM = 'health_potion_medium',
+  HEALTH_POTION_LARGE = 'health_potion_large',
 }
 
 export enum SpriteState {
@@ -86,15 +89,4 @@ export enum AnimatedSpriteID {
   CHOMPBUG_GREEN = 'chompbug_green',
   CHOMPBUG_RED = 'chompbug_red',
   CHOMPBUG_BLACK = 'chompbug_black',
-  SKELETON_GREEN = "SKELETON_GREEN",
-  SKELETON_BLUE = "SKELETON_BLUE",
-  SKELETON_RED = "SKELETON_RED",
-  HYDRA_RED = "HYDRA_RED",
-  GOBLIN_GREEN = "GOBLIN_GREEN",
-  GOBLIN_BLUE = "GOBLIN_BLUE",
-  ORC_RED = "ORC_RED",
-  IMP_GREEN = "IMP_GREEN",
-  IMP_BLUE = "IMP_BLUE",
-  IMP_RED = "IMP_RED",
-  OGRE_RED = "OGRE_RED",
 }
