3b222de8d1a76cd2c2f3b51b0106a576116c84d2
Dominic-Roser <domdomroser@gmail.com>
2024-11-11 19:22:18 -0800

    added template creator, and rearranged a bunch of stuff, synched xp with backend

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -4,14 +4,28 @@ import {
   TouchableOpacity,
   StyleSheet,
   View,
+  Modal
 } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+
 import { secondsToMinutes } from '@/utils/workout';
+import updateEXP from "@/services/workout"
+import { useUserStore } from '@/store/user'
+
+
+
 
 const Workout = () => {
   const [isWorkoutActive, setIsWorkoutActive] = useState(false);
   const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const { user } = useUserStore();
+
+  if(!user){
+    throw new Error('User data not found.');
+  }
+  const userID = user.id;
   const [timer, setTimer] = useState<ReturnType<typeof setInterval> | null>(
     null,
   );
@@ -19,8 +33,60 @@ const Workout = () => {
   type Exercise = { name: string; weight: number; reps: number; sets: number };
 
   const fillerex: Exercise = { name: 'Bench', weight: 135, reps: 20, sets: 4 };
+
+  const fillerexbench: Exercise = { name: 'Bench Press', weight: 135, reps: 20, sets: 4 };
+  const fillerexsquat: Exercise = { name: 'Squat', weight: 135, reps: 20, sets: 4 };
+  const fillerexlandmine: Exercise = { name: 'Landmine Press', weight: 135, reps: 20, sets: 4 };
+  const fillerexdeadlift: Exercise = { name: 'Deadlift', weight: 135, reps: 20, sets: 4 };
+  const fillerexpushup: Exercise = { name: 'Push Up', weight: 135, reps: 20, sets: 4 };
+  const fillerexpulldown: Exercise = { name: 'Lat Pulldown', weight: 135, reps: 20, sets: 4 };
+  const fillerexbicep: Exercise = { name: 'Bicep Curl', weight: 135, reps: 20, sets: 4 };
+  const fillerexercises: Exercise[] = [fillerexbench, fillerexbicep, fillerexdeadlift, fillerexlandmine, fillerexpulldown, fillerexpushup, fillerexsquat];
+
+
+  
   const fillerworkout = [fillerex, fillerex, fillerex, fillerex];
   const fillerworkoutsuggest = [fillerex, fillerex, fillerex];
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedExercises, setSelectedExercises] = useState<Exercise[]>([]);
+
+  // adds to selected
+  const addExercise = (exercise: Exercise) => {
+    console.log("Added "+exercise+" to selected exercises")
+    if (!selectedExercises.includes(exercise)) {
+      setSelectedExercises([...selectedExercises, exercise]);
+    }
+  };
+
+  // removes from selected
+  const removeExercise = (name: string) => {
+    console.log("Removed "+name+" from selected exercises")
+    setSelectedExercises(selectedExercises.filter((item) => item.name !== name));  
+  };
+
+  //finds exercise in exercises with name name
+  const findExercise  = (name: string, exercises: Exercise[]) => {
+    if(!exercises.map(exercise => exercise.name).includes(name)){
+      throw new Error("Exercise with name "+name+ " not found");
+    }
+    // Find the exercise by name in the exercises array
+    const exercise = exercises.find(exercise => exercise.name === name);
+
+    // If exercise is not found, throw an error
+    if (!exercise) {
+      throw new Error("Exercise with name " + name + " not found in exercises list");
+    }
+
+    // Return the found exercise
+    return exercise;   
+  }
+
+  const closeTemplateCreator = () => {
+    console.log("template creator closed");
+    setSelectedExercises([]);  
+    setModalVisible(false);
+  }
 
   const exerciseToString = (exercise: Exercise) => {
     return (
@@ -126,6 +192,7 @@ const Workout = () => {
     if (isWorkoutActive) {
       setIsWorkoutActive(false);
       if (timer) {
+        updateEXP(userID, secondsElapsed); //update user exp
         clearInterval(timer); // Stop the timer
         setTimer(null); // Clear the timer ID
       }
@@ -140,6 +207,86 @@ const Workout = () => {
       }
     };
   }, [timer]);
+
+  ///////////////////////// for template creation ///////////////////////////////
+
+  const CreateTemplateScreen = () => {    
+    return (
+      <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)} // Allow modal to close on hardware back button on Android
+      >
+      <View style={templatestyles.modalOverlay}>
+        <View style={[templatestyles.modalCreatorContent, { flex: 1 }]}>
+          <Text style={templatestyles.modalCreatorHeader}>Template Creator</Text>
+    
+          {/* List of Exercises */}
+          <View style={{ flex: 1, marginBottom: 10 }}> 
+          <Text style={templatestyles.sectionTitle}>Available Exercises</Text>
+
+            {/*names of each of the exercises*/}
+            <FlatList
+              data={fillerexercises.map(exercise => exercise.name)}
+              keyExtractor={(item) => item}
+              renderItem={({ item }) => (
+                <View style={templatestyles.exerciseItem}>
+                  <TouchableOpacity
+                    style={templatestyles.addButton}
+                    onPress={() => addExercise(findExercise(item,fillerexercises))}
+                  >
+                    <Text style={templatestyles.addButtonText}>+</Text>
+                  </TouchableOpacity>
+                  <Text>  {item}</Text>
+                </View>
+              )}
+              contentContainerStyle={{ paddingBottom: 20 }} // Padding to avoid cutoff at bottom
+            />
+          </View>
+    
+          {/* Selected Exercises Section (initially empty) */}
+          <Text style={templatestyles.sectionTitle}>Selected Exercises</Text>
+            <View style={{ flex: 1, marginBottom: 10, alignItems: 'flex-start'}}> 
+              <FlatList
+                data={selectedExercises.map(exercise => exercise.name)}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                  <View style={templatestyles.exerciseItem}>
+                    <TouchableOpacity
+                      style={templatestyles.removeButton}
+                      onPress={() => removeExercise(item)}
+                    >
+                      <Text style={templatestyles.addButtonText}>-</Text>
+                    </TouchableOpacity>
+                    <Text>  {item}</Text>
+                  </View>
+                )}
+                contentContainerStyle={{ paddingBottom: 20 }} // Padding to avoid cutoff at bottom
+              />
+            </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+            <TouchableOpacity
+              style={templatestyles.closeButton}
+              onPress={() => closeTemplateCreator()} // Close modal
+            >
+              <Text style={templatestyles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={templatestyles.saveButton}
+              onPress={() => setModalVisible(false)} // Close modal
+            >
+              <Text style={templatestyles.closeButtonText}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+    
+    );
+  };
+
+  ////////////////////// moving to return section //////////////////////
 
   return (
     <SafeAreaView className="flex-1 items-left justify-left h-full bg-offWhite px-6">
@@ -179,8 +326,9 @@ const Workout = () => {
                   marginTop: 15,
                   borderRadius: 40,
                 }}
-                onPress={() => console.log('create template pressed')}
+                onPress={() => setModalVisible(true)}
               >
+                {CreateTemplateScreen()}
                 <Text style={{ color: 'white', fontSize: 18 }}>
                   Create Template
                 </Text>
@@ -253,6 +401,10 @@ const templatestyles = StyleSheet.create({
   modalOverlay: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:60,
+    
   },
   dropdownContainer: {
     marginTop: 5,
@@ -273,6 +425,98 @@ const templatestyles = StyleSheet.create({
     paddingHorizontal: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  createButton: {
+    backgroundColor: 'purple',
+    width: 165,
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 40,
+    alignItems: 'center',
+  },
+  buttonCreatorText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  modalCreatorOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalCreatorContent: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'flex-start',
+  },
+  modalCreatorHeader: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  exerciseItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  exerciseText: {
+    fontSize: 16,
+    flex: 1,
+  },
+  addButton: {
+    backgroundColor: 'green',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  removeButton: {
+    backgroundColor: 'red',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  selectedExercise: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  noExercisesText: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  closeButton: {
+    backgroundColor: 'red',
+    padding: 15,
+    borderRadius: 20,
+    marginTop: 10,
+  },
+  saveButton: {
+    backgroundColor: 'purple',
+    padding: 15,
+    borderRadius: 20,
+    marginTop: 10,
+  },
+  closeButtonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
 
---
+++ services/workout.ts
@@ -0,0 +1,66 @@
+import {
+    doc,
+    getDoc,
+    updateDoc,
+    collection,
+    arrayUnion,
+    QueryDocumentSnapshot,
+} from 'firebase/firestore';
+  import { FIREBASE_DB } from '@/firebaseConfig';
+  import { APIResponse } from '@/types/general';
+  import { userConverter } from './user';
+
+  
+  // Function to handle the updating of exp
+  /**
+   * Purchase an item for the user.
+   * @param {string} userID - The user's unique ID.
+   * @param {number} duration - The duration worked out
+   * @returns {Promise<APIResponse>} Returns an APIResponse object.
+   */
+  const updateEXP: (
+    userID: string,
+    duration: number,
+  ) => Promise<APIResponse> = async (userID, duration) => {
+    try {
+        // // Re-add this potion after demo is done
+        // if (duration < 600) {
+        //     throw new Error('Not long enough workout');
+        // }
+        const userCollection = collection(FIREBASE_DB, 'users').withConverter(
+            userConverter,
+        );
+    
+        // Get user data
+        const userRef = doc(userCollection, userID);
+        const userSnap = await getDoc(userRef);
+        const userData = userSnap.data();
+
+        if (!userData) {
+            throw new Error('User data not found.');
+        }
+
+        // Get new user exp amount
+        const newEXP = userData.exp+duration;
+        await updateDoc(userRef, { exp: newEXP });
+
+        console.log("exp successfully incremented by "+duration+"xp.")
+
+      return {
+        data: null,
+        success: true,
+        error: null,
+      };
+    } catch (error) {
+      console.error('Error updating exp:', error);
+  
+      return {
+        data: null,
+        success: false,
+        error: 'Error updating exp.',
+      };
+    }
+  };
+  
+  export default updateEXP; //userID;
+  
