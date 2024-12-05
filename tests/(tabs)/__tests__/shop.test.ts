ddc3930fb999f46053322341d50823bbde47c7dc
Dominic-Roser <domdomroser@gmail.com>
2024-12-04 23:56:23 -0800

    added multiple tests for fail to buy and cancel

--- tests/(tabs)/__tests__/shop.test.ts
+++ tests/(tabs)/__tests__/shop.test.ts
@@ -2,19 +2,17 @@
 
 // import React from 'react';
 // import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import { render, fireEvent } from '@testing-library/react-native';
+import { screen, render, fireEvent } from '@testing-library/react-native';
 import Shop from '@/app/(tabs)/shop'; // Update path if necessary
 import React from 'react';
 // import { useUserStore } from '@/store/user';
 // import { useItemStore } from '@/store/item';
 // import { BASE_ITEM } from '@/constants/item';
 import { ItemType } from '@/types/item';
+import { purchaseItem } from '@/services/item';
+// import { useUserStore } from '@/store/user';
 // import { purchaseItem } from '@/services/item';
 // import { BASE_USER } from '@/constants/user';
-// Mock Ionicons
-jest.mock('@expo/vector-icons', () => ({
-  Ionicons: '',
-}));
 // Mock stores and services
 // jest.mock('@/store/user', () => ({
 //   useUserStore: jest.fn(),
@@ -54,30 +52,20 @@ jest.mock('expo-router', () => ({
 // Mock Zustand stores
 jest.mock('@/store/user', () => ({
   useUserStore: () => ({
-    user: { id: 'user-1', name: 'Test User' },
+    user: { id: 'user-1', name: 'Test User', gold: 60 },
     setUser: jest.fn(),
   }),
 }));
 
-jest.mock('@/store/workout', () => ({
-  useWorkoutStore: () => ({
-    workout: {
-      exercises: [],
-    },
-    setWorkout: jest.fn(),
-  }),
+jest.mock('@/store/item', () => ({
+  useItemStore: jest.fn(() => ({
+    items: mockItems, // Provide the mock items here
+  })),
 }));
-// jest.mock('react', () => ({
-//   useState: jest.fn(),
-// }));
 
-// Test data
-// const mockUser = {
-//   id: 'user-1',
-//   gold: 0,
-//   equipments: [],
-//   consumables: [],
-// };
+jest.mock('@/services/item', () => ({
+  purchaseItem: jest.fn(), //.mockResolvedValue({ success: true }), // Mock success response
+}));
 
 const mockItems = [
   {
@@ -101,12 +89,13 @@ const mockItems = [
     health: 20,
     spriteID: 'sprite-2',
     description: 'A sturdy shield.',
+    testId: 'Shield',
   },
   {
     id: 'item-3',
     name: 'Small Potion',
     type: ItemType.POTION_SMALL,
-    cost: 10,
+    cost: 70,
     power: 0,
     speed: 0,
     health: 5,
@@ -134,33 +123,84 @@ describe('Shop Component', () => {
     jest.clearAllMocks();
   });
   it('renders the Shop component with items', () => {
-    const { getByText } = render(React.createElement<typeof Shop>(Shop));
+    render(React.createElement<typeof Shop>(Shop));
     expect(true).toBe(true);
     // Check for Shop header and user gold
-    expect(getByText('Shop')).toBeTruthy();
-    expect(getByText(`0 Gold`)).toBeTruthy();
+    expect(screen.getByText('Shop')).toBeTruthy();
+    expect(screen.getByText(`60 Gold`)).toBeTruthy();
 
     // Check for item categories
-    expect(getByText('WEAPONS')).toBeTruthy();
-    expect(getByText('ARMORS')).toBeTruthy();
-    expect(getByText('ACCESSORIES')).toBeTruthy();
-    expect(getByText('POTIONS')).toBeTruthy();
-
-    // // Check for items
-    // expect(getByText('Sword')).toBeTruthy();
-    // expect(getByText('Shield')).toBeTruthy();
-    // expect(getByText('Small Potion')).toBeTruthy();
+    expect(screen.getByText('WEAPONS')).toBeTruthy();
+    expect(screen.getByText('ARMORS')).toBeTruthy();
+    expect(screen.getByText('ACCESSORIES')).toBeTruthy();
+    expect(screen.getByText('POTIONS')).toBeTruthy();
   });
 
-  it('opens and displays item details in a modal when an item is selected', () => {
+  it('opens and displays item details in a modal when an item is selected and goes back when canceled', () => {
     render(React.createElement<typeof Shop>(Shop));
-    const { getByText } = render(React.createElement<typeof Shop>(Shop));
-    fireEvent.press(getByText('Sword'));
+    // const { getByText } = render(React.createElement<typeof Shop>(Shop));
+    fireEvent.press(screen.getByText('50 Gold'));
+    expect(true).toBe(true);
+    // // Check for modal content
+    expect(screen.getByText('Equip Sword')).toBeTruthy();
+    expect(screen.getByText('CANCEL')).toBeTruthy();
+    expect(screen.getByText('BUY ITEM')).toBeTruthy();
+    expect(screen.getByText('A sharp sword.')).toBeTruthy();
+    expect(
+      screen.getByText(`${mockItems[0].cost} Gold to purchase`),
+    ).toBeTruthy();
 
-    // Check for modal content
-    expect(getByText('Equip Sword')).toBeTruthy();
-    expect(getByText('A sharp sword.')).toBeTruthy();
-    expect(getByText(`${mockItems[0].cost} Gold to purchase`)).toBeTruthy();
+    //should go back to main without decreasing money
+    fireEvent.press(screen.getByText('CANCEL'));
+    expect(screen.queryByText('Equip Sword')).toBeNull();
+    expect(screen.getByText('60 Gold')).toBeTruthy();
+  });
+
+  it('opens and displays item details then complete purchase when funds sufficient', async () => {
+    //TODO: implment user to finish correct buy
+    const mockSetUser = jest.fn();
+    (useUserStore as jest.Mock).mockReturnValue({
+      user: { id: 'user-1', name: 'Test User', gold: 50 },
+      setUser: mockSetUser,
+    });
+    render(React.createElement<typeof Shop>(Shop));
+    // const { getByText } = render(React.createElement<typeof Shop>(Shop));
+    fireEvent.press(screen.getByText('50 Gold'));
+    expect(true).toBe(true);
+    // // Check for modal content
+    expect(screen.getByText('Equip Sword')).toBeTruthy();
+    expect(screen.getByText('CANCEL')).toBeTruthy();
+    expect(screen.getByText('BUY ITEM')).toBeTruthy();
+    expect(screen.getByText('A sharp sword.')).toBeTruthy();
+    expect(
+      screen.getByText(`${mockItems[0].cost} Gold to purchase`),
+    ).toBeTruthy();
+
+    (purchaseItem as jest.Mock).mockResolvedValue({ success: false });
+    // should fail to purchase when insufficient funds
+    fireEvent.press(screen.getByText('BUY ITEM'));
+    expect(screen.getByText('10 Gold')).toBeTruthy();
+    //expect(screen.queryByText('10 Gold')).toBeTruthy();
+    //expect(screen.getByText('70 Gold')).toBeTruthy();
+  });
+
+  it('opens and displays item details then doesnt complete purchase when not enough funds', () => {
+    render(React.createElement<typeof Shop>(Shop));
+    // const { getByText } = render(React.createElement<typeof Shop>(Shop));
+    fireEvent.press(screen.getByText('70 Gold'));
+    expect(true).toBe(true);
+    // // Check for modal content
+    expect(screen.getByText('Equip Small Potion')).toBeTruthy();
+    expect(screen.getByText('CANCEL')).toBeTruthy();
+    expect(screen.getByText('BUY ITEM')).toBeTruthy();
+    expect(screen.getByText('Restores a small amount of health.')).toBeTruthy();
+    expect(
+      screen.getByText(`${mockItems[2].cost} Gold to purchase`),
+    ).toBeTruthy();
+
+    // should fail to purchase when insufficient funds
+    fireEvent.press(screen.getByText('BUY ITEM'));
+    expect(screen.getByText('Equip Small Potion')).toBeTruthy();
   });
 
   // it('shows an alert if the user does not have enough gold to purchase an item', async () => {
